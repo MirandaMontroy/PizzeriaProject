c8df464a174509a04b539132279bcb8b92027353
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-05-04 00:09:52 -0500

    customization finalized

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images',blank=True)
 
     def __str__(self):
         return self.name
